251e988e0194399420fe84828483996fbaafd994
maccopacco <maxdmax101@gmail.com>
2019-03-02 15:28:39 -0500

    Added Face to Feeder Station (Same side) #50

--- src/main/java/frc/robot/auto/commands/AutoModeBuilder.java
+++ src/main/java/frc/robot/auto/commands/AutoModeBuilder.java
@@ -8,7 +8,14 @@ import frc.robot.GZOI;
 import frc.robot.auto.commands.functions.drive.pathfollowing.PathContainer;
 import frc.robot.auto.commands.paths.left.Left_CS_Bay_1_Opp;
 import frc.robot.auto.commands.paths.left.Left_CS_Bay_1_Same;
+import frc.robot.auto.commands.paths.left.Left_CS_Face_Opp;
+import frc.robot.auto.commands.paths.left.Left_CS_Face_Same;
+import frc.robot.auto.commands.paths.to_feeder_station.CS_Face_Turn_Around_Opp;
+import frc.robot.auto.commands.paths.to_feeder_station.CS_Face_Turn_Around_Same;
+import frc.robot.auto.commands.paths.to_feeder_station.To_Feeder_Station_Same;
+import frc.robot.auto.commands.paths.to_feeder_station.To_Feeder_Station_Same_Shallow;
 import frc.robot.auto.commands.paths.center.Center_CS_Bay_1_Left;
+import frc.robot.auto.commands.paths.center.Center_CS_Face_Left;
 import frc.robot.util.GZCommand;
 import frc.robot.util.GZCommandGroup;
 
@@ -49,6 +56,21 @@ public class AutoModeBuilder {
         }
     }
 
+    public enum GamePiece {
+        CARGO, HATCH_PANEL
+    }
+
+    public enum FeederStation {
+        LEFT(true), RIGHT(false);
+        private final boolean onLeft;
+        private final boolean onRight;
+
+        private FeederStation(boolean onLeft) {
+            this.onLeft = onLeft;
+            this.onRight = !this.onLeft;
+        }
+    }
+
     public enum ScoringSide {
         LEFT(true), RIGHT(false);
         private final boolean onLeft;
@@ -64,6 +86,14 @@ public class AutoModeBuilder {
         return scoringSameSide(startPos, location.side);
     }
 
+    public static boolean feederSameSide(ScoringLocation location, FeederStation station) {
+        if (location.side == ScoringSide.LEFT && station == FeederStation.LEFT)
+            return true;
+        else if (location.side == ScoringSide.RIGHT && station == FeederStation.RIGHT)
+            return true;
+        return false;
+    }
+
     public static boolean scoringSameSide(StartingPosition startPos, ScoringSide side) {
         if (startPos == StartingPosition.LEFT && side == ScoringSide.LEFT)
             return true;
@@ -74,16 +104,21 @@ public class AutoModeBuilder {
         return false;
     }
 
-    public enum GamePiece {
-        CARGO, HATCH_PANEL
-    }
-
-    public enum FeederStation {
-        LEFT, RIGHT;
-    }
-
     public static ArrayList<PathContainer> getFirstPath(StartingPosition startPos, ScoringLocation score) {
         switch (score.pos) {
+        case CARGO_SHIP_FACE:
+            // Center
+            if (startPos == StartingPosition.CENTER) {
+                return new Center_CS_Face_Left().get(score.side.onLeft).toList();
+            }
+
+            // On left or right
+            if (scoringSameSide(startPos, score)) {
+                return new Left_CS_Face_Same().get(startPos.onLeft).toList();
+            } else {
+                return new Left_CS_Face_Opp().get(startPos.onLeft).toList();
+            }
+
         case CARGO_SHIP_BAY_1:
             // Center
             if (startPos == StartingPosition.CENTER) {
@@ -121,6 +156,18 @@ public class AutoModeBuilder {
 
     public static ArrayList<PathContainer> getScoredPosToFeederStation(final ScoringLocation location,
             final FeederStation station) {
+        switch (location.pos) {
+        case CARGO_SHIP_FACE:
+
+            ArrayList<PathContainer> ret = new ArrayList<>();
+            if (feederSameSide(location, station)) {
+                ret.add(new CS_Face_Turn_Around_Same().get(location.side.onLeft));
+            } else {
+                ret.add(new CS_Face_Turn_Around_Opp().get(location.side.onLeft));
+            }
+            ret.add(new To_Feeder_Station_Same().get(station.onLeft));
+            return ret;
+        }
         return null;
     }
 
