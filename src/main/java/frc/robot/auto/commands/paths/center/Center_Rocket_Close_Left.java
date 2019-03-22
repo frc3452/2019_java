d90dcf0e2db87a297ab1e7f8ca225d547a7f773e
maccopacco <maxdmax101@gmail.com>
2019-03-22 19:05:30 -0400

    Added rocket path adaption to all rocket paths :D

--- src/main/java/frc/robot/auto/commands/paths/center/Center_Rocket_Close_Left.java
+++ src/main/java/frc/robot/auto/commands/paths/center/Center_Rocket_Close_Left.java
@@ -3,6 +3,7 @@ package frc.robot.auto.commands.paths.center;
 import java.util.ArrayList;
 
 import frc.robot.auto.commands.functions.drive.pathfollowing.PathBuilder.Waypoint;
+import frc.robot.auto.pathadapter.PathAdapter;
 import frc.robot.auto.commands.functions.drive.pathfollowing.PathContainer;
 
 public class Center_Rocket_Close_Left extends PathContainer {
@@ -11,9 +12,10 @@ public class Center_Rocket_Close_Left extends PathContainer {
         sWaypoints.add(new Waypoint(67.5, 162, 0, 0));
         sWaypoints.add(new Waypoint(120, 162, 15, 30));
         sWaypoints.add(new Waypoint(141, 255, 15, 60));
-        sWaypoints.add(new Waypoint(172.325, 283.165, 15, 60)/*.setFieldAdaption(- Rocket -)*/);
-        sWaypoints.add(new Waypoint(184.89750408828039, 290.0624925480436, 1, 30)/*.setFieldAdaption(- Rocket -)*/);
-        sWaypoints.add(new Waypoint(197.47, 296.96, 0, 30)/*.setFieldAdaption(- Rocket -)*/);
+        sWaypoints.add(new Waypoint(172.325, 283.165, 15, 60).setFieldAdaption(PathAdapter.rocketNear));
+        sWaypoints.add(new Waypoint(184.89750408828039, 290.0624925480436, 1, 30).setFieldAdaption(PathAdapter.rocketNear));
+        sWaypoints.add(new Waypoint(197.47, 296.96, 0, 30).setFieldAdaption(PathAdapter.rocketNear
+        ));
     }
 
     @Override
