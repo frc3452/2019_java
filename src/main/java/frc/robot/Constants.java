f233223e2f7b2f6439d11fb87c6beb2b46a65390
maccopacco <maxdmax101@gmail.com>
2019-04-06 14:50:10 -0400

    tuning to motor checker

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,7 +24,7 @@ import frc.robot.util.drivers.pneumatics.GZSolenoid.SolenoidConstants;
  */
 
 public class Constants {
-	public static final boolean COMP_BOT = true;
+	public static final boolean COMP_BOT = false;
 
 	public static class kLoop {
 	}
