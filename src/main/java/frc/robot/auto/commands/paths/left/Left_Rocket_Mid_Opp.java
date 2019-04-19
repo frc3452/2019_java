9237dab05d3e31a4c0ef220c30808de667f4d609
maccopacco <maxdmax101@gmail.com>
2019-04-18 20:43:31 -0400

    Updated all left paths to take advantage of turning in place

--- src/main/java/frc/robot/auto/commands/paths/left/Left_Rocket_Mid_Opp.java
+++ src/main/java/frc/robot/auto/commands/paths/left/Left_Rocket_Mid_Opp.java
@@ -9,7 +9,7 @@ public class Left_Rocket_Mid_Opp extends PathContainer {
     public Left_Rocket_Mid_Opp() {
         this.sWaypoints = new ArrayList<Waypoint>();
         sWaypoints.add(new Waypoint(67.5, 205, 0, 0));
-        sWaypoints.add(new Waypoint(153, 205, 15, 30));
+        sWaypoints.add(new Waypoint(130, 205, 15, 30));
         sWaypoints.add(new Waypoint(204, 120, 25, 60));
         sWaypoints.add(new Waypoint(229.28, 80.32300000000001, 15, 60).setFieldAdaption(PathAdapter.rocketMid));
         sWaypoints.add(new Waypoint(229.28, 63.13150000000002, 1, 30).setFieldAdaption(PathAdapter.rocketMid));
