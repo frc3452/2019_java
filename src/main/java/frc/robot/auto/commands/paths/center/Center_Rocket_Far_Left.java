905b3665211cf7449ce246069cf0caf7992a8971
maccopacco <maxdmax101@gmail.com>
2019-04-19 20:10:34 -0400

    Updated all center paths

--- src/main/java/frc/robot/auto/commands/paths/center/Center_Rocket_Far_Left.java
+++ src/main/java/frc/robot/auto/commands/paths/center/Center_Rocket_Far_Left.java
@@ -1,25 +1,28 @@
 package frc.robot.auto.commands.paths.center;
 
 import java.util.ArrayList;
-
+import frc.robot.auto.commands.functions.drive.pathfollowing.PathContainer;
 import frc.robot.auto.commands.functions.drive.pathfollowing.PathBuilder.Waypoint;
 import frc.robot.auto.pathadapter.PathAdapter;
-import frc.robot.auto.commands.functions.drive.pathfollowing.PathContainer;
+import frc.robot.poofs.util.math.Rotation2d;
 
 public class Center_Rocket_Far_Left extends PathContainer {
     public Center_Rocket_Far_Left() {
         this.sWaypoints = new ArrayList<Waypoint>();
         sWaypoints.add(new Waypoint(67.5, 162, 0, 0));
         sWaypoints.add(new Waypoint(120, 162, 15, 30));
-        sWaypoints.add(new Waypoint(205, 203, 15, 60));
-        sWaypoints.add(new Waypoint(291, 245, 15, 60));
-        sWaypoints.add(new Waypoint(289.973, 280.95, 15, 60).setFieldAdaption(PathAdapter.rocketFar));
-        sWaypoints.add(new Waypoint(275.3814278800546, 288.95486854129393, 1, 30).setFieldAdaption(PathAdapter.rocketFar));
-        sWaypoints.add(new Waypoint(260.79, 296.96, 0, 30).setFieldAdaption(PathAdapter.rocketFar));
+        sWaypoints.add(new Waypoint(215, 240, 15, 60));
+        sWaypoints.add(new Waypoint(251, 270, 15, 60));
+        sWaypoints.add(new Waypoint(275.3814278800546, 288.95486854129393, 0, 30).setFieldAdaption(PathAdapter.rocketFar));
     }
 
     @Override
     public boolean isReversed() {
         return false;
+    }
+
+    @Override
+    public Rotation2d getEndGyroMovement() {
+        return Rotation2d.fromDegrees(270 - 61.25);
     }
 }
