0d6fff7b8a96adf7d03039d5155b476941d18426
maccopacco <maxdmax101@gmail.com>
2019-04-22 22:09:29 -0400

    work work work

--- src/main/java/frc/robot/ConfigurableDrive/ConfigurableDrive.java
+++ src/main/java/frc/robot/ConfigurableDrive/ConfigurableDrive.java
@@ -14,9 +14,43 @@ public class ConfigurableDrive {
     private Button s_upTick;
     private Button s_downTick;
 
+    // private
+
     private ArrayList<DriveStyle> mStyles = new ArrayList<DriveStyle>();
+    private final double kARCADE_DEADBAND = 0.05;
 
     public ConfigurableDrive() {
+    }
+
+    public void addDriveStyle(DriveStyle style) {
+        this.mStyles.add(style);
+    }
+
+    public void addTankDrive() {
+        GZJoystick j = new GZJoystick(0);
+        this.addDriveStyle(new DriveStyle("Tank", () -> j.getLeftAnalogY(), () -> j.getRightAnalogY()) {
+
+            @Override
+            public DriveSignal produceDriveSignal() {
+                double left = getAxis(1);
+                double right = getAxis(2);
+
+                return new DriveSignal(left, right);
+            }
+        });
+    }
+
+    public void addOneStickArcade() {
+        GZJoystick j = new GZJoystick(0);
+        this.addDriveStyle(new DriveStyle("One stick arcade", () -> j.getLeftAnalogY(), () -> j.getLeftAnalogX()) {
+
+            @Override
+            public DriveSignal produceDriveSignal() {
+                DriveSignal output = arcade(getAxis(1), getAxis(2), false);
+
+                return output;
+            }
+        });
     }
 
     public abstract static class DriveStyle {
@@ -27,13 +61,16 @@ public class ConfigurableDrive {
          * 
          * @param axises
          */
+        @SafeVarargs
         public DriveStyle(String name, Supplier<Double>... axises) {
             this.axises = axises;
             this.name = name;
         }
 
+        public abstract DriveSignal produceDriveSignal();
+
         /**
-         * First axis passed will be axis 1, NOT 0
+         * First axis passed will be axis 1, not 0
          * 
          * @param axis
          * @return
@@ -42,15 +79,17 @@ public class ConfigurableDrive {
             if (axis < 1) {
                 throwError("Axis " + axis + " less than 1; first axis starts at 1!");
                 return 0;
-            } 
+            }
 
-            if (axis > 3);
-        
+            if (axis >= axises.length) {
+                throwError("Axis " + axis + " too high, this mode was only supplied " + axises.length + " axises!");
+                return 0;
+            }
+
             return axises[axis - 1].get();
         }
 
-        private void throwError(String message)
-        {
+        private void throwError(String message) {
             System.out.println("ERROR ConfigurableDrive option [" + toString() + "]" + message);
         }
 
@@ -75,6 +114,76 @@ public class ConfigurableDrive {
 
         public boolean updated() {
             return mLB.update(get());
+        }
+    }
+
+    public synchronized DriveSignal arcade(double xSpeed, double zRotation, boolean squaredInputs) {
+        return arcade(xSpeed, zRotation, squaredInputs, kARCADE_DEADBAND);
+    }
+
+    public synchronized DriveSignal arcade(double xSpeed, double zRotation, boolean squaredInputs, double deadband) {
+        xSpeed = limit1to1(xSpeed);
+        xSpeed = applyDeadband(xSpeed, deadband);
+
+        zRotation = limit1to1(zRotation);
+        zRotation = applyDeadband(zRotation, deadband);
+
+        double leftMotorOutput;
+        double rightMotorOutput;
+
+        // Square the inputs (while preserving the sign) to increase fine control
+        // while permitting full power.
+        if (squaredInputs) {
+            xSpeed = Math.copySign(xSpeed * xSpeed, xSpeed);
+            zRotation = Math.copySign(zRotation * zRotation, zRotation);
+        }
+
+        double maxInput = Math.copySign(Math.max(Math.abs(xSpeed), Math.abs(zRotation)), xSpeed);
+
+        if (xSpeed >= 0.0) {
+            // First quadrant, else second quadrant
+            if (zRotation >= 0.0) {
+                leftMotorOutput = maxInput;
+                rightMotorOutput = xSpeed - zRotation;
+            } else {
+                leftMotorOutput = xSpeed + zRotation;
+                rightMotorOutput = maxInput;
+            }
+        } else {
+            // Third quadrant, else fourth quadrant
+            if (zRotation >= 0.0) {
+                leftMotorOutput = xSpeed + zRotation;
+                rightMotorOutput = maxInput;
+            } else {
+                leftMotorOutput = maxInput;
+                rightMotorOutput = xSpeed - zRotation;
+            }
+        }
+
+        DriveSignal retval = new DriveSignal(limit1to1(leftMotorOutput), -limit1to1(rightMotorOutput));
+
+        return retval;
+    }
+
+    public static double limit1to1(double value) {
+        if (value > 1.0) {
+            return 1.0;
+        }
+        if (value < -1.0) {
+            return -1.0;
+        }
+        return value;
+    }
+
+    public static double applyDeadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                return (value - deadband) / (1.0 - deadband);
+            } else {
+                return (value + deadband) / (1.0 - deadband);
+            }
+        } else {
+            return 0.0;
         }
     }
 
---
+++ src/main/java/frc/robot/ConfigurableDrive/DriveSignal.java
@@ -0,0 +1,29 @@
+package frc.robot.ConfigurableDrive;
+
+/**
+ * A drivetrain command consisting of the left, right motor values
+ */
+public class DriveSignal {
+    protected double mLeftMotor;
+    protected double mRightMotor;
+
+    public DriveSignal(double left, double right) {
+        mLeftMotor = left;
+        mRightMotor = right;
+    }
+
+    public static DriveSignal NEUTRAL = new DriveSignal(0, 0);
+
+    public double getLeft() {
+        return mLeftMotor;
+    }
+
+    public double getRight() {
+        return mRightMotor;
+    }
+
+    @Override
+    public String toString() {
+        return "L: " + mLeftMotor + ", R: " + mRightMotor;
+    }
+}
