5d2aa1db6974cfba617f717039e998bc818b3231
maccopacco <maxdmax101@gmail.com>
2019-05-21 14:47:02 -0400

    junit testing

---
+++ src/test/main/java/frc/robot/TestTest.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import org.junit.Test;
+
+public class TestTest {
+    @Test
+    void test(){
+        double a = 5.0;
+        double b = 1.0;
+        assertEquals(a < b, "Zero less than one");
+        System.out.println("DIE");
+    }
+}
