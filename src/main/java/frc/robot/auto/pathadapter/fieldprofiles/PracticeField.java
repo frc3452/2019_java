66de350de98b33491ec75bb68b434c22de03e4cc
maccopacco <maxdmax101@gmail.com>
2019-07-09 18:55:20 -0400

    updating heights
    Signed-off-by: maccopacco <maxdmax101@gmail.com>

--- src/main/java/frc/robot/auto/pathadapter/fieldprofiles/PracticeField.java
+++ src/main/java/frc/robot/auto/pathadapter/fieldprofiles/PracticeField.java
@@ -3,22 +3,6 @@ package frc.robot.auto.pathadapter.fieldprofiles;
 import frc.robot.Constants;
 
 public class PracticeField extends FieldProfile {
-    public PracticeField() {
-        this.mFeederStationToDriverWall = new FieldValues<Double>(0.0);
-        this.mFeederStationToWall = new FieldValues<Double>(25.72);
-        this.mBay1ToWall = new FieldValues<Double>(133.13);
-        this.mBay2ToWall = new FieldValues<Double>(133.13);
-        this.mBay3ToWall = new FieldValues<Double>(133.13);
-        this.mBay1ToRamp = new FieldValues<Double>(163.52);
-        this.mBay2ToRamp = new FieldValues<Double>(207.02);
-        this.mBay3ToRamp = new FieldValues<Double>(250.52);
-        this.mCargoShipFaceToHABRamp = new FieldValues<Double>(123.97);
-        this.mCargoShipFaceToMidLine = new FieldValues<Double>(10.875);
-        this.mHABLevel1EdgeToRamp = new FieldValues<Double>(40.7387);
-        this.mHABLevel3EdgeToDriverStation = new FieldValues<Double>(40.0);
-        this.mHABRamp = new FieldValues<Double>(10.9632);
-    }
-
     private HeightsContainer mCompHeightsAtWorlds = new HeightsContainer() {
         @Override
         public double hp_floor_Grab() {
@@ -27,17 +11,17 @@ public class PracticeField extends FieldProfile {
 
         @Override
         public double hp1() {
-            return 20;
+            return 20 + 3;
         }
 
         @Override
         public double hp2() {
-            return 47;
+            return 47 + 3;
         }
 
         @Override
         public double hp3() {
-            return 75;
+            return 75 + 3;
         }
 
         @Override
@@ -90,6 +74,22 @@ public class PracticeField extends FieldProfile {
             return 3;
         }
     };
+
+    public PracticeField() {
+        this.mFeederStationToDriverWall = new FieldValues<Double>(0.0);
+        this.mFeederStationToWall = new FieldValues<Double>(25.72);
+        this.mBay1ToWall = new FieldValues<Double>(133.13);
+        this.mBay2ToWall = new FieldValues<Double>(133.13);
+        this.mBay3ToWall = new FieldValues<Double>(133.13);
+        this.mBay1ToRamp = new FieldValues<Double>(163.52);
+        this.mBay2ToRamp = new FieldValues<Double>(207.02);
+        this.mBay3ToRamp = new FieldValues<Double>(250.52);
+        this.mCargoShipFaceToHABRamp = new FieldValues<Double>(123.97);
+        this.mCargoShipFaceToMidLine = new FieldValues<Double>(10.875);
+        this.mHABLevel1EdgeToRamp = new FieldValues<Double>(40.7387);
+        this.mHABLevel3EdgeToDriverStation = new FieldValues<Double>(40.0);
+        this.mHABRamp = new FieldValues<Double>(10.9632);
+    }
 
     @Override
     public HeightsContainer getElevatorHeights() {
